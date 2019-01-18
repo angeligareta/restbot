4fae7e48b85026afa11c54bde7ab96f8f21a55e3
Ángel Igareta <32129522+angeligareta@users.noreply.github.com>
2019-01-18 10:30:42 +0000

    Added Intent Handler

---
+++ app/src/main/java/com/example/chatbot/IntentHandler.kt
@@ -0,0 +1,58 @@
+package com.example.chatbot
+
+
+import android.util.Log
+import com.google.gson.JsonElement
+import java.util.HashMap
+
+/**
+ * Object that handles the complex dialog flow intents.
+ */
+object IntentHandler {
+
+    private val TAG = "IntentHandler"
+    private var dailyMenu : ArrayList<String> = ArrayList()
+
+    /**
+     * Method that handle the intent received by parameter.
+     * It works by looking for a method in IntentHandler that has the name of the intent.
+     * If it exists, it would be called with the intentParameters.
+     */
+    fun handleIntent(intentName: String?, intentParameters: HashMap<String, JsonElement>){
+        try {
+            val method = this.javaClass.getMethod(intentName, intentParameters.javaClass)
+            method.invoke(this, intentParameters)
+        }
+        catch (error : Exception) {
+            // Could notify for further intent implementations
+            Log.d(TAG, "Intent $intentName does not have a method")
+        }
+    }
+
+    /** METHODS FOR EACH COMPLEX DIALOGFLOW INTENT */
+    fun getDailyMenu(intentParameters : HashMap<String, JsonElement>) {
+        Speaker.play("El menu del dia es ")
+        for (food in dailyMenu) {
+            Speaker.play(food)
+        }
+        Log.d(TAG, "GET DAILY MENU REACHED $intentParameters")
+    }
+
+    fun setDailyMenu(intentParameters : HashMap<String, JsonElement>) {
+        Log.d(TAG, "SET DAILY MENU REACHED $intentParameters")
+
+        val foodList = intentParameters["food"]
+        foodList!!.asJsonArray.forEach { food ->
+            dailyMenu.add(food.asString)
+        }
+    }
+
+    fun getBill(intentParameters : HashMap<String, JsonElement>) {
+        Log.d(TAG, "GET BILL REACHED $intentParameters")
+    }
+
+    fun askForFood(intentParameters : HashMap<String, JsonElement>) {
+        Log.d(TAG, "ASK FOR FOOD $intentParameters")
+    }
+
+}
